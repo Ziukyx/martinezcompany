9192e32cf9d6bef1273e3cdf2e039bcb11acad8c
aimming <ainaprogramming@gmail.com>
2024-01-21 01:31:06 +0100

    ok

--- src/Components/english/Contact.jsx
+++ src/Components/english/Contact.jsx
@@ -1,4 +1,3 @@
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 import FeaturesDesktop from './Mobile'
 import { FadeIn } from '../FadeIn'
 
@@ -31,7 +30,9 @@ export default function Contact() {
               <div className="flex gap-x-4 xxs:justify-center lg:justify-normal">
                 <dt className="flex-none">
                   <span className="sr-only ">Telephone</span>
-                  <PhoneIcon className="xxs:h-7 xxs:w-6 md:h-8 md:w-7 text-blue-950 dark:text-white" aria-hidden="true" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className='xxs:h-7 xxs:w-6 md:h-8 md:w-7 text-blue-950 dark:text-white'>
+                      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21a12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"/>
+                    </svg>
                 </dt>
                 <dd>
                   <a className="text-blue-950 hover:text-blue-300 dark:hover:text-gray-500 dark:text-white texto" href="tel:+34 623 954 897">
@@ -42,7 +43,9 @@ export default function Contact() {
               <div className="flex gap-x-4 xxs:justify-center lg:justify-normal">
                 <dt className="flex-none">
                   <span className="sr-only">Email</span>
-                  <EnvelopeIcon className="xxs:h-7 xxs:w-6 md:h-10 md:w-8 text-blue-950 dark:text-white" aria-hidden="true" />
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className='xxs:h-7 xxs:w-6 md:h-10 md:w-8 text-blue-950 dark:text-white'>
+                    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"/>
+                  </svg>
                 </dt>
                 <dd>
                   <a className="text-blue-950 hover:text-blue-300 dark:hover:text-gray-500 dark:text-white texto" href="mailto:admin@martinezcompany.eu">
